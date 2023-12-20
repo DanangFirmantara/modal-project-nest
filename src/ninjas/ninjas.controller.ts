b75aa4f095d6d441a3eb69824c308d4deb1135be
Danang Firmantara <danangfirmantara837@gmail.com>
2023-12-20 21:39:47 +0700

    add guard and validation data post

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,16 +1,13 @@
-import { BadGatewayException, BadRequestException, Body, ConflictException, Controller, Delete, ForbiddenException, GatewayTimeoutException, Get, GoneException, HttpVersionNotSupportedException, ImATeapotException, InternalServerErrorException, MethodNotAllowedException, NotAcceptableException, NotFoundException, NotImplementedException, Param, PayloadTooLargeException, Post, PreconditionFailedException, Put, Query, RequestTimeoutException, ServiceUnavailableException, UnauthorizedException, UnprocessableEntityException, UnsupportedMediaTypeException } from '@nestjs/common';
+import { Body, Controller, Delete,  Get, NotFoundException, Param, ParseIntPipe, Post, Put, Query, UseGuards, ValidationPipe } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
-import { NotFoundError } from 'rxjs';
-
-//depedencies injection
-// const service = new NinjasService()
-// const controller = new NinjasController(service)
+import { BeltGuard } from 'src/belt/belt.guard';
 
 @Controller('ninjas')
+@UseGuards(BeltGuard)
 export class NinjasController {
-    constructor(private readonly ninjaService: NinjasService){};
+    constructor(private readonly ninjaService: NinjasService) { };
 
     //GET /ninjas?weapon=fast --> []
     @Get()
@@ -20,27 +17,28 @@ export class NinjasController {
 
     //GET /ninjas/:id --> { ... }
     @Get(':id')
-    getOneNinja(@Param('id') id:string){
-        try{
-            return this.ninjaService.getNinja(+id);
-        } catch(err){
+    getOneNinja(@Param('id', ParseIntPipe) id: number) {
+        try {
+            return this.ninjaService.getNinja(id);
+        } catch (err) {
             throw new NotFoundException()
         }
     }
 
     //POST /ninjas
     @Post()
-    createNinja(@Body() createNinjaDto: CreateNinjaDto){
+    @UseGuards(BeltGuard)
+    createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
         return this.ninjaService.createNinja(createNinjaDto)
     }
     //PUT /ninjas/:id --> { ... }
     @Put(':id')
-    updateNinja(@Param('id') id:string, @Body() updateNinjaDto: UpdateNinjaDto){
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
         return this.ninjaService.updateNinja(+id, updateNinjaDto)
     }
     //DELETE /ninjas/:id
     @Delete(':id')
-    removeNinja(@Param('id') id:string){
+    removeNinja(@Param('id') id: string) {
         return this.ninjaService.removeNinja(+id)
     }
 
